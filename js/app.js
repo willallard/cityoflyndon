ac5a27cf1746a49773e09f574b9e91c352f00d10
willallard <willallard@Will-Allards-MacBook-Pro.local>
2016-03-13 20:12:21 -0400

    Form updates!

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 
+  angular.module('submitExample', [])
+    .controller('ExampleController', ['$scope', function($scope) {
+      $scope.list = [];
+      $scope.text = 'hello';
+      $scope.submit = function() {
+        if ($scope.text) {
+          $scope.list.push(this.text);
+          $scope.text = '';
+        }
+      };
+    }]);
+
 
