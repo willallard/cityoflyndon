88d715bc57e81b6fef4eede26eeff730e5b495eb
willallard <willallard@Will-Allards-MacBook-Pro.local>
2016-03-17 14:23:03 -0400

    updates!

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
-
-  angular.module('submitExample', [])
-    .controller('ExampleController', ['$scope', function($scope) {
+angular.module('submitExample', [])
+    .controller('ExampleController', ['$scope', function ($scope) {
       $scope.list = [];
       $scope.text = 'hello';
+      $scope.name = '';
       $scope.submit = function() {
         if ($scope.text) {
           $scope.list.push(this.text);
           $scope.text = '';
-        }
+          }
       };
+        
+        $scope.submit = function() {
+    // set form is submitted
+    $scope.submitted = false;
+
+    if ($scope.someForm.$invalid) {
+      return;
+    }
+
+
+    $scope.newData = {};
+
+    // set form default state
+    $scope.someForm.$setPristine();
+
+    // set form is no submitted
+    $scope.submitted = true;
+
+    return;
+  }
+    
+        
+        
     }]);
-
-
