10583e8f34d48a56927d61e202a07857e93d100a
willallard <willallard@dhcp012179.code-louisville-wifi.lmcdc.local>
2016-03-17 19:40:44 -0400

    Class updates

--- js/app.js
+++ js/app.js
@@ -9,27 +9,5 @@ angular.module('submitExample', [])
           $scope.text = '';
           }
       };
-        
-        $scope.submit = function() {
-    // set form is submitted
-    $scope.submitted = false;
+}]);
 
-    if ($scope.someForm.$invalid) {
-      return;
-    }
-
-
-    $scope.newData = {};
-
-    // set form default state
-    $scope.someForm.$setPristine();
-
-    // set form is no submitted
-    $scope.submitted = true;
-
-    return;
-  }
-    
-        
-        
-    }]);
