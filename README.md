81d97aff35a4794ee45f5f1d94459bc3e2ad4393
willallard <willallard@Will-Allards-MacBook-Pro.local>
2016-03-24 14:36:57 -0400

    Finalizing!

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my project for Code Louisville! I am working on a website for the City of Lyndon.
+This is my project for Code Louisville! I am working on a website for the City of Lyndon. 
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* REPORT A PROBLEM */
+
 angular.module('submitExample', [])
     .controller('ExampleController', ['$scope', function ($scope) {
       $scope.list = [];
@@ -11,3 +13,60 @@ angular.module('submitExample', [])
       };
 }]);
 
+/* CALENDAR */
+
+$.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.min.js',function(){
+  
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+  
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'null'    
+      /*right: 'month,agendaWeek,agendaDay'*/
+    },
+    editable: true,
+    events: [
+      {
+        
+        title: 'Shakespeare In The Park',
+        start: new Date(y, m, d-3, 18, 0),
+        allDay: false
+      },
+      
+      {
+        title: 'City Meeting',
+        start: new Date(y, m, d, 10, 30),
+        allDay: false
+      },
+      {
+        title: 'Car Wash Fundraiser',
+        start: new Date(y, m, d, 12, 0),
+        end: new Date(y, m, d, 14, 0),
+        allDay: false
+      },
+      {
+        title: 'Code Louisville Project Due',
+        start: new Date(y, m, d+1, 12, 0),
+        end: new Date(y, m, d+1, 15, 0),
+        allDay: false
+      },
+      {
+       title: 'Graduation Mixer',
+       start: new Date(y, m, d+14, 17, 30),
+       end: new Date(y, m, d+14, 19, 0),
+       allDay: false
+      },
+      {
+       title: 'Spring Festival at Robsion Park',
+       start: new Date(y, m, d+16, 8, 0),
+       end: new Date(y, m,d+16, 22, 30 ),
+       allDay: false      
+      }
+    ]
+  });
+})
